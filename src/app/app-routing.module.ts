7483d16fe4fff938bd29a4c975e620f2fb4d5632
Labeeb Latheef <labeebklatif@gmail.com>
2022-06-16 13:04:17 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AwayComponent } from './away/away.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'away', component: AwayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+// @ts-nocheck
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'widget-angular';
+
+  ngOnInit() {
+    (function () {
+      var w = window,
+        d = document,
+        z = w.NeetoChat || {};
+      z.subdomain = 'spinkart';
+      z.chatApiKey = 'kWJCAyH4JbWF3Opx';
+      z.neetoChatBaseUrl = 'https://spinkart.neetochat.net';
+      z.neetoKbBaseUrl = 'https://spinkart.neetokb.net';
+      z.neetoDeskBaseUrl = 'https://spinkart.neetodesk.net';
+      w.NeetoChat = z;
+      var inserted = false;
+      function insertTag() {
+        if (inserted) return;
+        inserted = true;
+        var s = d.createElement('script');
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src =
+          'https://neeto-chat-widget-production-v1.s3.amazonaws.com/bundle-loader.js';
+        var x = d.getElementsByTagName('script')[0];
+        x.parentNode.insertBefore(s, x);
+      }
+      if (document.readyState != 'loading') {
+        insertTag();
+      } else {
+        document.onreadystatechange = function () {
+          if (document.readyState != 'loading') insertTag();
+        };
+      }
+    })();
+  }
 }
